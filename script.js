104f0ac73da0002adc682039a12936f4bc223beb
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-21 10:09:01 +0300

    learning canvas patterns

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const canvasEle = document.getElementById("my-first-canvas");
-const img = document.getElementById("react-logo");
+const img = document.getElementById("img");
 
 const ctx = canvasEle.getContext("2d");
 const color = "#b81616";
@@ -11,5 +11,7 @@ gradient.addColorStop(0, "red");
 gradient.addColorStop(0.5, "white");
 gradient.addColorStop(1, "green");
 
-ctx.fillStyle = gradient;
+const pattern = ctx.createPattern(img, "no-repeat")
+
+ctx.fillStyle = pattern;
 ctx.fillRect(0, 0, canvasEle.width, canvasEle.height);
