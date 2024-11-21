37ce175d1020db8adc41afda4acb8870fb032333
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-21 10:04:04 +0300

    learning: linear gradient

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const canvasEle = document.getElementById("my-first-canvas");
+const img = document.getElementById("react-logo");
 
 const ctx = canvasEle.getContext("2d");
+const color = "#b81616";
+const gradient = ctx.createLinearGradient(0, 0, canvasEle.width, 0);
 
-ctx.fillStyle = "#b81616";
-ctx.fillRect(0, 0, canvasEle.width / 2, canvasEle.height / 2);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(0.5, "white");
+gradient.addColorStop(1, "green");
+
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, canvasEle.width, canvasEle.height);
