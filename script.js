5de018bf314ad8e0b6e7fc88b390558e54391c70
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-21 09:41:38 +0300

    init: canvas context

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-'use strict'
+"use strict";
 
+const canvasEle = document.getElementById("my-first-canvas");
+
+const ctx = canvasEle.getContext("2d");
+
+ctx.fillStyle = "#b81616";
+ctx.fillRect(0, 0, canvasEle.width / 2, canvasEle.height / 2);
