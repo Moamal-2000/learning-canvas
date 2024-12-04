136b84ab6759a3e76e2ec34acb11f3a264641e49
Moamal-2000 <moamalalaapro1@gmail.com>
2024-12-05 00:36:02 +0300

    fix: clearRect before start draw the canvas to remove the previous draw on re-rendering

--- src/Components/Shared/Canvas.jsx
+++ src/Components/Shared/Canvas.jsx
@@ -16,15 +16,23 @@ function writeCanvas(canvas) {
   const height = canvas.height;
   const width = canvas.width;
 
+  ctx.clearRect(0, 0, width, height);
+
+  drawGrid(ctx, width, height, 10);
+}
+
+function drawGrid(ctx, width, height, scale = 20) {
   ctx.strokeStyle = "#d6d6d6";
 
-  for (let x = 0.5; x < height; x += 20) {
+  for (let x = 0.5; x < height; x += scale) {
+    ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, height);
     ctx.stroke();
   }
 
-  for (let y = 0.5; y < width; y += 20) {
+  for (let y = 0.5; y < width; y += scale) {
+    ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(width, y);
     ctx.stroke();
